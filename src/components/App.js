df56d12642a54efb081aa1f75274d076d3e5c294
inesantos29 <ines.arts@hotmail.com>
2020-04-14 18:49:13 +0100

    Authentication

--- src/components/App.js
+++ src/components/App.js
@@ -122,6 +122,7 @@ class App extends Component {
           deleteFish={this.deleteFish}
           loadSampleFishes={this.loadSampleFishes}
           fishes={fishes}
+          storeId={this.props.match.params.storeId}
         />
       </div>
     )
--- src/utils/base.js
+++ src/utils/base.js
@@ -9,4 +9,8 @@ const firebaseApp = firebase.initializeApp({
 
 const base = Rebase.createClass(firebaseApp.database())
 
+// This is a named export
+export { firebaseApp }
+
+// This is a default export
 export default base
